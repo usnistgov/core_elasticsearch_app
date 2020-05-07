3f6ec44f73a1ff0f68e298221c215f8ec031a592
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2020-05-07 17:01:07 -0400

    fix: catch exception instead of testing for None
    Change-Id: I0b68f3366539a644a00cb6d4c1a3108d4504cc85

--- core_elasticsearch_app/components/data/elasticsearch.py
+++ core_elasticsearch_app/components/data/elasticsearch.py
@@ -2,12 +2,12 @@
 """
 import logging
 
-from core_elasticsearch_app.commons.exceptions import ElasticsearchError
 from core_elasticsearch_app.components.data.autocomplete_settings import DATA_AUTOCOMPLETE_SETTINGS
 from core_elasticsearch_app.components.elasticsearch_template import api as elasticsearch_template_api
 from core_elasticsearch_app.settings import ELASTICSEARCH_CDCS_DATA_INDEX
 from core_elasticsearch_app.utils.elasticsearch_client import ElasticsearchClient
 from core_elasticsearch_app.utils.utils import get_nested_value
+from core_main_app.commons import exceptions
 
 logger = logging.getLogger(__name__)
 
@@ -30,8 +30,8 @@ def index_data(data):
     Returns:
 
     """
-    es_template = elasticsearch_template_api.get_by_template(data.template)
-    if es_template:
+    try:
+        es_template = elasticsearch_template_api.get_by_template(data.template)
         es_data = {
             'data_id': str(data.id),
             'title': get_value_from_path(data, es_template.title_path),
@@ -39,6 +39,10 @@ def index_data(data):
         }
         # TODO: could use global PID instead of id
         return ElasticsearchClient.index_document(ELASTICSEARCH_CDCS_DATA_INDEX, str(data.id), es_data)
+    except exceptions.DoesNotExist:
+        logger.warning("Data with id {0} will not be indexed. Not template configured.".format(str(data.id)))
+    except Exception as e:
+        logger.error(str(e))
 
 
 def get_suggestions(query, fuzziness=1, prefix_length=3, fragment_size=100):
