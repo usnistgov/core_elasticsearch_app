95399678559d0b709dff2f971271d1716d936356
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2020-09-01 08:41:54 -0400

    docs: add settings doc
    Change-Id: I7a5c5486ea010b9f91a0db0b161102be7e5095cb

--- core_elasticsearch_app/settings.py
+++ core_elasticsearch_app/settings.py
@@ -1,5 +1,7 @@
 """ Settings for core_elasticsearch_app.
-These settings are overwritten at project level.
+
+Settings with the following syntax can be overwritten at the project level:
+SETTING_NAME = getattr(settings, "SETTING_NAME", "Default Value")
 """
 from django.conf import settings
 
